2890a1b91af2a1d209c26b1ad50bbfbee14de707
bearsanity <jsweeney96@hotmail.com>
2026-01-22 01:59:41 -0330

    added post route for new notes

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const crypto = require('crypto');
 const app = express();
 
+const DB_PATH  = path.join(__dirname, 'db.json'); //path to notes database. written like this because it is a TRUE constant
 
 //===================== Middleware =====================
 app.use('/assets', express.static(path.join(__dirname, 'public')));
@@ -20,4 +21,15 @@ app.get('/api/notes', (req, res) => {
     const notesData = fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8');
     const notesDataParsed = JSON.parse(notesData);
     res.json(notesDataParsed);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    const newNote = req.body;
+    newNote.id = crypto.randomUUID();
+    const notesArray = fs.readFileSync(DB_PATH, 'utf8');
+    const notesArrayParsed = JSON.parse(notesArray);
+    notesArrayParsed.push(newNote);
+    const newArray = JSON.stringify(notesArrayParsed);
+    fs.writeFileSync(DB_PATH, newArray);
 });
